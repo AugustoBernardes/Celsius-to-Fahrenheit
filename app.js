8fac47ec4c2456c1fa9acd67bd933ea2d2d55f18
Augusto Bernardes <augustobdev@outlook.com>
2021-04-07 07:27:19 -0300

    Fixing a function

--- app.js
+++ app.js
@@ -17,43 +17,48 @@ function calculateCelsius(){
         let calculate = (valueCelsius * 9/5) + 32 
         
         console.log(calculate)
-
-        // Temeprature Low
-        if (valueCelsius <= 35){
-            html.innerHTML = `
-         <div class="image">
-            <img src="./assets/neve.svg">
-         </div>
-         <p>Your temperature is ${calculate.toFixed(1)} Fahrenheit, your temperature is low!</p>
-        `
-        }
-        // Temeprature High
-        if(valueCelsius >= 37.5){
-            html.innerHTML = `
-         <div class="image">
-            <img src="./assets/fogo.svg">
-         </div>
-         <p>Your temperature is ${calculate.toFixed(1)} Fahrenheit, your temperature is high!</p>
-        `
-        }
-        // Temeprature Normal
-        if (valueCelsius > 35 && valueCelsius < 37.5){
-            html.innerHTML = `
-            <div class="image">
-               <img src="./assets/normal.svg">
-            </div>
-            <p>Your temperature is ${calculate.toFixed(1)} Fahrenheit, your temperature is normal!</p>
-           `
-        }
-        // Temeprature Normal
-        if (valueCelsius <= 10 || valueCelsius >= 42 ){
-            html.innerHTML = `
-            <div class="image">
-               <img src="./assets/dead.svg">
-            </div>
-            <p>Your temperature is ${calculate.toFixed(1)} Fahrenheit, you are DEAD ("RIP!")</p>
-           `
-        }
+      checkTemperature(calculate)
+     
     }
    
+}
+
+
+function checkTemperature(valueFahrenheit){
+      // Temeprature Low
+      if (valueFahrenheit <= 95){
+         html.innerHTML = `
+      <div class="image">
+         <img src="./assets/neve.svg">
+      </div>
+      <p>Your temperature is ${valueFahrenheit.toFixed(1)} Fahrenheit, your temperature is low!</p>
+     `
+     }
+     // Temeprature High
+     if(valueFahrenheit >= 99.5){
+         html.innerHTML = `
+      <div class="image">
+         <img src="./assets/fogo.svg">
+      </div>
+      <p>Your temperature is ${valueFahrenheit.toFixed(1)} Fahrenheit, your temperature is high!</p>
+     `
+     }
+     // Temeprature Normal
+     if (valueFahrenheit > 95 && valueFahrenheit < 99.5){
+         html.innerHTML = `
+         <div class="image">
+            <img src="./assets/normal.svg">
+         </div>
+         <p>Your temperature is ${valueFahrenheit.toFixed(1)} Fahrenheit, your temperature is normal!</p>
+        `
+     }
+     // Temeprature Normal
+     if (valueFahrenheit <= 50 || valueFahrenheit >= 107.6 ){
+         html.innerHTML = `
+         <div class="image">
+            <img src="./assets/dead.svg">
+         </div>
+         <p>Your temperature is ${valueFahrenheit.toFixed(1)} Fahrenheit, your DEAD ("RIP!")</p>
+        `
+     }
 }
